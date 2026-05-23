9b43c3368658126a9a330f94dcf37708f6214b20
Anshul Mahajan <harshalsharma101@gmail.com>
2026-05-24 01:23:43 +0530

    Jupyter notebook done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    author = 'anshulmahajan',
+    author_email = 'mahajan.anshul.01@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+
+)
